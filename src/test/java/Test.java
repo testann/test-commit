2aedb129be91a1d1760dad5a519923e896b6e85c
anna.saklakova <saklakova.anya@gmail.com>
2019-09-30 19:57:44 +0300

    Trying to reproduced

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -2,9 +2,10 @@ public class Test {
     private final Integer myInt;
     
     public Test(Integer myInt) {
+    
         assert myInt != null : "myInt != null";
     
-        this.myInt = myInt + 123;
+        this.myInt = myInt + 1234;
         
         //test
     }
