40fc334f68b226edfaa5c730404c2ee173ad203d
anna.saklakova <saklakova.anya@gmail.com>
2019-10-01 14:40:08 +0300

    tmp

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -2,10 +2,13 @@ public class Test {
   private final Integer myInt;
   
   public Test(Integer myInt) {
-    
+  
     assert myInt != null : "myInt != null";
+  
     this.myInt = myInt + 123;
-    
+  
+  
+    //new comment
     //test new comm
   }
 }
