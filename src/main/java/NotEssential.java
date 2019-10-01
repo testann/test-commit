5301496edeaf7276b436c97402b304726bf8d3ae
anna.saklakova <saklakova.anya@gmail.com>
2019-10-01 16:47:55 +0300

    qwe

--- src/main/java/NotEssential.java
+++ src/main/java/NotEssential.java
@@ -1,23 +1,23 @@
 public class NotEssential {
-     
-     public void callMe(int p1, int p2, int p3, int p4, int p5, int p6, int p7, int p8, int p9, int pA, int pB, int pC, int pD, int pE) {
-     
-     }
-     
-     public void callIt() {
-          callMe(1234567890,
-            1234567890,
-            1234567890,
-            1234567890,
-            1234567890,
-            1234567891,
-            1234567890,
-            1234567122,
-            1234567001,
-            1234567111,
-            1234567000,
-            1234567000,
-            1234567111,
-            1234567000);
-     }
+   
+   public void callMe(int p1, int p2, int p3, int p4, int p5, int p6, int p7, int p8, int p9, int pA, int pB, int pC, int pD, int pE) {
+   
+   }
+   
+   public void callIt() {
+      callMe(1234567890,
+              1234567890,
+              1234567890,
+              1234567890,
+              1234567890,
+              1234567891,
+              1234567890,
+              1234567122,
+              1234567001,
+              1234567111,
+              1234567000,
+              1234567000,
+              1234567111,
+              1234567000);
+   }
 }
