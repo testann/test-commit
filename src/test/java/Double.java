3473fd67aa2e8af9328c5b469598e2b3e525d74e
anna.saklakova <saklakova.anya@gmail.com>
2020-03-17 15:22:52 +0300

    Return

--- src/test/java/Double.java
+++ src/test/java/Double.java
@@ -3,4 +3,26 @@ public class Double {
     int X;
     int Y;
 
+    /*
+
+Elle ne peut donc, à elle seule, nourrir le débat qui doit sengager en Europe et, au-delà, dans les pays candidats.
+Les motifs personnels se rapportent surtout aux changements d’emploi ou de lieu de travail qui rendent plus au moins nécessaire le déménagement.
+Chemins de traverse se veut différent mais il reste ancré dans lEcole.
+Marchez sur la plate-forme verte pour arriver à la pièce suivante.
+Monsieur MINETTE à en charge la construction de l’abris de la pompe à incendie et doit le faire avant juin 2002.
+Peu de tourisme de masse donc en Tanzanie faute de facilités d’hébergement.
+Quand passe la demoiselle.
+En revanche, des littoraux anciennement touristiques connaissent des baisses localisées de capacité daccueil (dans les Bouches-du-Rhône, une partie des Alpes Maritimes ou du Morbihan).
+La livraison est réputée effectuée par la délivrance au Client du produit commandé.
+Conformation extérieure (dissections d’encéphales de Chien et Chat)
+Le DESS Image Numérique et Interactivité ainsi que le DEA Sciences de l’Information et de la communication leur sont également accessibles.
+Il portait des sandales noires. - Approche et enlèves moi les sandales...
+Dîner tyrolien, nuit à l’hôtel.
+Elle a souvent la bouche ouverte.
+
+     */
+
+    void test(){
+        //test
+    }
 }
